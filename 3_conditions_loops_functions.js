dce6b2e26bcb75478bee4b55257ee47a56dd90ba
Nathan Alabi <alabinathan1916@gmail.com>
2022-09-03 16:53:44 +0100

    conditionals

---
+++ 3_conditions_loops_functions.js
@@ -0,0 +1,33 @@
+/* 
+if-else statements-----------------------------------------------------------------
+      if ("Nathan" === "Nathan"){
+        console.log('equal strings..');
+      }
+      else{
+        console.log("not equal strings..");
+      }
+
+Switch Statements-----------------------------------------------------------------
+      
+
+
+
+*/
+
+
+// if, else if, else example-------------------------------------------------------
+// const x = "Money";
+// const y = x.length;
+
+// if (y > 10){
+//   console.log("\nChecks out..");
+// }
+// else if (x == "Money"){
+//   console.log("\nThis is Money.");
+// }
+// else{
+//   console.log("dosent checks out.");
+// }
+
+// Switch examples................................................................
+
