a6582fff0bc31fc2aa33390d973c4434505aa72b
Nathan Alabi <NathanAlabi@AZTECMFB.Local>
2022-09-03 16:41:06 +0100

    variables and data types

---
+++ 1_variables_and_data_types.js
@@ -0,0 +1,46 @@
+/* --------------> variables declaration and assignment.
+1  const: is used when you want to assign a permanent value to an object.
+          when using const: the value assigned never change!
+2  let: is used when you want to be able to reassign values to objects or      variables
+
+
+-----------------> Data Types.
+1: nubmer
+2: string
+3: boolean
+example: 
+        const variable1=10; const variable2="Money"; const variable3=false;
+to get data type:
+        typeof variable1 --> results number.
+        typeof variable2 --> results string.
+        typeof variable3 --> results boolean.
+Note: ---> Number() is just like int() function in python --> ie, you can convert string into integer/Number to perform mathematical operations.
+4: Array
+exmple:
+      const array1 = [10, "name", {"boy":100}, [20, 44]];
+  assessed by indexing: array1[0]; --> results 10. etc.
+
+
+5: Objects in Javascript.
+Objects in JavaScript is a key-value pair, just like dictionary in python.
+example: 
+        const objectVariable = {prop1:20, prop2:50, prop3:70};
+  accessed by either:
+        objectVariable.prop1; --> result is 20
+  or:
+        objectVariable["prop1"]; --> result is 20
+        objectVariable["prop2"]; --> result is 50
+
+
+
+----------------------------> Functions in JavaScript.
+example 1:
+        const my_function_1 = function(){
+          return 30;
+        }
+
+
+
+*/
+
+console.log("One\nTwo");
