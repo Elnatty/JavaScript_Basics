bd0d8716dbab9961f920e07a7779db7028901b96
Nathan Alabi <NathanAlabi@AZTECMFB.Local>
2022-09-12 17:00:27 +0100

    callback in javascript

---
+++ 4_callBacks_and_assynchronous_programming.js
@@ -0,0 +1,27 @@
+/*
+==========================================================================
+============================== CallBacks =================================
+CallBacks in javascript enables code function reusability.
+example 1:
+        const myArray = [2,3,4,5,6];
+        // a callback function.
+        function myOperation(item){
+          return item * 2;
+        }
+        // to call the function..
+        const newArray = myArray.map(myOperation);
+        // console.log(newArray) -> Outputs [4,6,8,10,12]
+
+example 2:
+        const strArray = ["Hello", "my", "name", "is", "Dking"];
+        // using arrow function to execute callback..
+        console.log(strArray.map(index_ => index_[0])) -> Outputs ['H', 'm', 'n', 'i', 'D']
+
+
+==========================================================================
+======================= Assynchronous Programming ========================
+Assynchronous Programming: code that does not run immediately.
+
+
+*/
+
