9fd6f67a1532c2f835c7da5630240a2a1f3cc2f0
Nathan Alabi <alabinathan1916@gmail.com>
2022-09-25 20:22:18 +0100

    Math method, generating random numbers

---
+++ 7_mathMethods.js
@@ -0,0 +1,7 @@
+/*
+finding a random element in an array=============================================
+const arr = ['this', 'is', 'super', 'story'];
+// to get a random value from d array
+const randIndex = Math.floor(arr.length * Math.random());
+console.log(arr[randIndex])
+*/
