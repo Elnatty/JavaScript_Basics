85c0ae2426027eb8218e2fa37294e5e01c7b2c49
Nathan Alabi <alabinathan1916@gmail.com>
2022-09-25 20:20:25 +0100

    regex with strings

---
+++ 6_string_methodReGEX.js
@@ -0,0 +1,12 @@
+/*
+Example 1.............................................................
+const str1 = "My Dog jumped on the bed. My dog is a bad Dog";
+// using the replaceAll method with regExp.
+console.log(str1.replaceAll(/[Dd]{1}og/g, "cat"));
+
+Example 2.............................................................
+const x = "<i> Should be italics </i>"; 
+// replacing i with h1 tag.
+console.log(x.replaceAll(/[i]>/g, "h1>"));
+
+*/
